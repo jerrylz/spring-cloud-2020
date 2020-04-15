56fec02fef374e7502877e7888614adf8fd8ab68
jerrylz <123>
2020-04-15 23:35:08 +0800

    提取服务公共代码，各服务引入common包。将订单服务端口改为6001（80端口在mac有冲突），修复application-dev.yml配置问题

--- cloud-consumer-order80/src/main/java/com/jerrylz/OrderMain6001.java
+++ cloud-consumer-order80/src/main/java/com/jerrylz/OrderMain6001.java
@@ -2,16 +2,15 @@ package com.jerrylz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.annotation.Order;
 
 /**
  * @author jerrylz
  * @date 2020/4/15
  */
 @SpringBootApplication
-public class OrderMain80 {
+public class OrderMain6001 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class, args);
+        SpringApplication.run(OrderMain6001.class, args);
     }
 }
 
